6a9b957e78bd23d6106eed6622a2948a8baba3a3
codemeleon <anmol.kiran@gmail.com>
2017-03-19 09:32:29 +0200

    Modified folder fixes

--- clalnviewer.py
+++ clalnviewer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""Main file for the task."""
 
 import click
 from clalnview import viewer
@@ -16,7 +17,10 @@ from os import path, makedirs
 @click.option("--undocount", help="Number of recoorections",
               type=int,
               default=20, show_default=True)
-def run(alnpath, alntype, undocount):
+@click.option("--modified", help="Folder for modified file",
+              type=str,
+              default="modified", show_default=True)
+def run(alnpath, alntype, undocount, modified):
     """Commandline Sequence alignemnt viewer."""
     """Please prefer to use it in full screen mode."""
     if not alnpath or not path.exists(alnpath):
@@ -26,8 +30,12 @@ def run(alnpath, alntype, undocount):
     if path.isfile(alnpath):
         hidden_path = path.split(alnpath)[0] + "/.alnview"
         file_list = [alnpath]
+        if modified == "modified":
+            modified = path.split(alnpath)[0] + "/" + modified
     elif path.isdir(alnpath):
         hidden_path = alnpath + "/.alnview"
+        if modified == "modified":
+            modified = path.split(alnpath)[0] + "/" + modified
         file_list = glob("%s/*" % alnpath)
     else:
         click.echo("Stranger thing(s). Don't know what to do.")
@@ -36,10 +44,11 @@ def run(alnpath, alntype, undocount):
         makedirs(hidden_path)
     if len(file_list):
         viewer.show_me_the_alignment(file_list, hidden_path,
-                                     alntype, undocount)
+                                     alntype, undocount, modified)
     else:
         click.echo("No input file found in give folder")
         exit(1)
+
 
 if __name__ == '__main__':
     run()
